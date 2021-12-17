88438ca8f70e3522c573acfd445e1562dbd8ad87
CoolSanya <Zhyhy123@gmail.com>
2021-12-17 20:55:59 +0200

    fix and add example croper in register index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.min.css";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "cropperjs/dist/cropper.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
